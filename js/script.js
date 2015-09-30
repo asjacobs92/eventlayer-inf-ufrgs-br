88533817fadca9a8ec1ce97753e2f37483bc8782
asjacobs <asjacobs.cic@gmail.com>
2015-09-30 02:02:07 -0300

    Esqueleto inicial do projeto.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function initialize() {
+		var mapCanvas = document.getElementById('events-map');
+		var mapOptions = {
+		    center: new google.maps.LatLng(-30.068473, -51.120434),
+		    zoom: 18,
+		    mapTypeId: google.maps.MapTypeId.ROADMAP
+		  }
+
+		var map = new google.maps.Map(mapCanvas, mapOptions);
+}

1b21d0f80ca6c609694ced55289e6096029e4e6b
asjacobs <asjacobs.cic@gmail.com>
2015-10-15 01:14:32 -0300

    Event list added (temporary, just as poc). Event popup now opens on mouse over.

--- js/script.js
+++ js/script.js
@@ -21,34 +21,196 @@ var eventList =
 		"Description":"Workshop sobre diferentes tipos de WebServices.",
 		"Longitude":-51.120520,
 		"Latitude":-30.068685
+	},
+	{
+		"EventId":3,
+		"EventTitle":"Feira de Oportunidade da UFRGS",
+		"EventType":"Palestra",
+		"EventImage":"resource/logo.png",
+		"TimeStart":"2014-02-05 15:25:23",
+		"TimeEnd":"2014-02-28 14:29:48",
+		"Description":"Feira para palestras de empresas interessadas na carne fresca da UFRGS.",
+		"Longitude":-51.121261,
+		"Latitude":-30.068719
+	},
+	{
+		"EventId":4,
+		"EventTitle":"Workshop de WebServices",
+		"EventType":"Workshop",
+		"EventImage":"resource/logo2.gif",
+		"TimeStart":"2014-02-05 00:00:00",
+		"TimeEnd":"2014-02-17 00:00:00",
+		"Description":"Workshop sobre diferentes tipos de WebServices.",
+		"Longitude":-51.120520,
+		"Latitude":-30.068685
+	},
+	{
+		"EventId":5,
+		"EventTitle":"Feira de Oportunidade da UFRGS",
+		"EventType":"Palestra",
+		"EventImage":"resource/logo.png",
+		"TimeStart":"2014-02-05 15:25:23",
+		"TimeEnd":"2014-02-28 14:29:48",
+		"Description":"Feira para palestras de empresas interessadas na carne fresca da UFRGS.",
+		"Longitude":-51.121261,
+		"Latitude":-30.068719
+	},
+	{
+		"EventId":6,
+		"EventTitle":"Workshop de WebServices",
+		"EventType":"Workshop",
+		"EventImage":"resource/logo2.gif",
+		"TimeStart":"2014-02-05 00:00:00",
+		"TimeEnd":"2014-02-17 00:00:00",
+		"Description":"Workshop sobre diferentes tipos de WebServices.",
+		"Longitude":-51.120520,
+		"Latitude":-30.068685
+	},
+	{
+		"EventId":7,
+		"EventTitle":"Feira de Oportunidade da UFRGS",
+		"EventType":"Palestra",
+		"EventImage":"resource/logo.png",
+		"TimeStart":"2014-02-05 15:25:23",
+		"TimeEnd":"2014-02-28 14:29:48",
+		"Description":"Feira para palestras de empresas interessadas na carne fresca da UFRGS.",
+		"Longitude":-51.121261,
+		"Latitude":-30.068719
+	},
+	{
+		"EventId":8,
+		"EventTitle":"Workshop de WebServices",
+		"EventType":"Workshop",
+		"EventImage":"resource/logo2.gif",
+		"TimeStart":"2014-02-05 00:00:00",
+		"TimeEnd":"2014-02-17 00:00:00",
+		"Description":"Workshop sobre diferentes tipos de WebServices.",
+		"Longitude":-51.120520,
+		"Latitude":-30.068685
+	},
+	{
+		"EventId":9,
+		"EventTitle":"Feira de Oportunidade da UFRGS",
+		"EventType":"Palestra",
+		"EventImage":"resource/logo.png",
+		"TimeStart":"2014-02-05 15:25:23",
+		"TimeEnd":"2014-02-28 14:29:48",
+		"Description":"Feira para palestras de empresas interessadas na carne fresca da UFRGS.",
+		"Longitude":-51.121261,
+		"Latitude":-30.068719
+	},
+	{
+		"EventId":10,
+		"EventTitle":"Workshop de WebServices",
+		"EventType":"Workshop",
+		"EventImage":"resource/logo2.gif",
+		"TimeStart":"2014-02-05 00:00:00",
+		"TimeEnd":"2014-02-17 00:00:00",
+		"Description":"Workshop sobre diferentes tipos de WebServices.",
+		"Longitude":-51.120520,
+		"Latitude":-30.068685
+	},
+	{
+		"EventId":11,
+		"EventTitle":"Feira de Oportunidade da UFRGS",
+		"EventType":"Palestra",
+		"EventImage":"resource/logo.png",
+		"TimeStart":"2014-02-05 15:25:23",
+		"TimeEnd":"2014-02-28 14:29:48",
+		"Description":"Feira para palestras de empresas interessadas na carne fresca da UFRGS.",
+		"Longitude":-51.121261,
+		"Latitude":-30.068719
+	},
+	{
+		"EventId":12,
+		"EventTitle":"Workshop de WebServices",
+		"EventType":"Workshop",
+		"EventImage":"resource/logo2.gif",
+		"TimeStart":"2014-02-05 00:00:00",
+		"TimeEnd":"2014-02-17 00:00:00",
+		"Description":"Workshop sobre diferentes tipos de WebServices.",
+		"Longitude":-51.120520,
+		"Latitude":-30.068685
+	},
+	{
+		"EventId":13,
+		"EventTitle":"Feira de Oportunidade da UFRGS",
+		"EventType":"Palestra",
+		"EventImage":"resource/logo.png",
+		"TimeStart":"2014-02-05 15:25:23",
+		"TimeEnd":"2014-02-28 14:29:48",
+		"Description":"Feira para palestras de empresas interessadas na carne fresca da UFRGS.",
+		"Longitude":-51.121261,
+		"Latitude":-30.068719
+	},
+	{
+		"EventId":14,
+		"EventTitle":"Workshop de WebServices",
+		"EventType":"Workshop",
+		"EventImage":"resource/logo2.gif",
+		"TimeStart":"2014-02-05 00:00:00",
+		"TimeEnd":"2014-02-17 00:00:00",
+		"Description":"Workshop sobre diferentes tipos de WebServices.",
+		"Longitude":-51.120520,
+		"Latitude":-30.068685
+	},
+	{
+		"EventId":15,
+		"EventTitle":"Feira de Oportunidade da UFRGS",
+		"EventType":"Palestra",
+		"EventImage":"resource/logo.png",
+		"TimeStart":"2014-02-05 15:25:23",
+		"TimeEnd":"2014-02-28 14:29:48",
+		"Description":"Feira para palestras de empresas interessadas na carne fresca da UFRGS.",
+		"Longitude":-51.121261,
+		"Latitude":-30.068719
+	},
+	{
+		"EventId":16,
+		"EventTitle":"Workshop de WebServices",
+		"EventType":"Workshop",
+		"EventImage":"resource/logo2.gif",
+		"TimeStart":"2014-02-05 00:00:00",
+		"TimeEnd":"2014-02-17 00:00:00",
+		"Description":"Workshop sobre diferentes tipos de WebServices.",
+		"Longitude":-51.120520,
+		"Latitude":-30.068685
 	}
 ];
 
+var map;
 function initialize() {
-		var mapCanvas = document.getElementById("events-map");
-		var mapOptions = {
-		    center: new google.maps.LatLng(-30.068473, -51.120434),
-		    zoom: 18,
-		    mapTypeId: google.maps.MapTypeId.ROADMAP,
-				mapTypeControl: false
-		  }
+	initializeMap();
+	initializeEvents();
+}
 
-		var map = new google.maps.Map(mapCanvas, mapOptions);
+function initializeMap() {
+	var mapCanvas = document.getElementById("events-map");
+	var mapOptions = {
+			center: new google.maps.LatLng(-30.068473, -51.120434),
+			zoom: 18,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			mapTypeControl: false
+		}
+	map = new google.maps.Map(mapCanvas, mapOptions)
+}
 
-		var infowindow = new google.maps.InfoWindow(), marker, i;
-		for (i = 0; i < eventList.length; i++) {
-			var start = eventList[i].TimeStart.split(" ");
-			var startDate = start[0];
-			var startTime = start[1];
+function initializeEvents() {
+	var infowindow = new google.maps.InfoWindow(), marker, i;
+	for (i = 0; i < eventList.length; i++) {
+		var start = eventList[i].TimeStart.split(" ");
+		var startDate = start[0];
+		var startTime = start[1];
 
-			var end = eventList[i].TimeEnd.split(" ");
-			var endDate = end[0];
-			var endTime = end[1];
+		var end = eventList[i].TimeEnd.split(" ");
+		var endDate = end[0];
+		var endTime = end[1];
 
-			var popupContent =
-				"<form action='index.php' method='post'>" +
-					"<input type='text' value='" + eventList[i].EventId +"' hidden name='eventId'/>" +
-			 		"<table id='eventPopup'>" +
+		var eventInfo =
+			"<form action='index.php' method='post'>" +
+				"<input type='text' value='" + eventList[i].EventId +"' hidden name='eventId'/>" +
+				"<div class='table-responsive'>" +
+					"<table id='eventPopup' class='table borderless'>" +
 						"<tr>" +
 							"<th style='text-align:center;' colspan='4'> <strong>" + eventList[i].EventTitle + "</strong> </th>" +
 							"<td colspan='2' style='text-align:right;'>" + eventList[i].EventType + "</td>" +
@@ -56,28 +218,38 @@ function initialize() {
 						"<tr>" +
 							"<td class='inp' colspan='1' style='text-align:right;'> Início em </td>" +
 							"<td class='inp' colspan='3' style='text-align:center;'> <strong>" + startDate + "</strong> às <strong>"+ startTime + "</strong> </td>" +
-							"<td colspan='1' rowspan='3'> <img border='solid 1px' src='"+ eventList[i].EventImage +"' height='150px' style='padding-left:15px'> </td>" +
+							"<td colspan='1' rowspan='3'> <img id='event"+ i +"' border='solid 1px' src='"+ eventList[i].EventImage +"' height='100px' style='padding-left:15px'> </td>" +
 						"</tr>" +
 						"<tr>" +
 							"<td class='inp' colspan='1'> até </td><td class='inp' colspan='3' style='text-align:center;'> <strong>" + endDate + "</strong> às <strong>" + endTime + "</strong></td>" +
 						"</tr>" +
 						"<tr>" +
-							"<td class='inp' colspan='1'> <strong> Descrição </strong> </td> <td colspan='3' style='max-width:300px; padding-left:10px'>" + eventList[i].Description + "</td>" +
+							"<td class='inp' colspan='1'> <strong> Descrição </strong> </td> <td colspan='3' style='max-width:200px; padding-left:10px'>" + eventList[i].Description + "</td>" +
 						"</tr>"+
 					"</table>" +
-				"</form>";
+				"</div>" +
+			"</form>";
 
-			marker = new google.maps.Marker({
-		    position: new google.maps.LatLng(eventList[i].Latitude, eventList[i].Longitude),
-		    map: map,
-		    title: eventList[i].EventTitle
-	  	});
+		$('#events-list').append(("<li href='#' class='list-group-item' style='padding:1%;'>" + eventInfo + "</li>"));	//events on list
+		$('#event' + i).css({ height: '100px' });
 
-			google.maps.event.addListener(marker, 'click', (function(marker, popupContent) {
-        return function() {
-            infowindow.setContent(popupContent);
-            infowindow.open(map, marker);
-        }
-    	}) (marker, popupContent));
-		}
+		marker = new google.maps.Marker({
+			position: new google.maps.LatLng(eventList[i].Latitude, eventList[i].Longitude),
+			map: map,
+			title: eventList[i].EventTitle
+		});
+
+		google.maps.event.addListener(marker, 'mouseover', (function(marker, eventInfo) {		//markers on map
+			return function() {
+					infowindow.setContent(eventInfo);
+					infowindow.open(map, marker);
+			}
+		}) (marker, eventInfo));
+
+		google.maps.event.addListener(marker, 'mouseout', (function(marker, eventInfo) {		//markers on map
+			return function() {
+					infowindow.close();
+			}
+		}) (marker, eventInfo));
+	}
 }
